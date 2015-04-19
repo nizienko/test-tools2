00171113b81432444fabfd6f27eeabd49a1e5134
eugene <nizienko@outlook.com>
2015-04-19 23:58:01 +0300

    next

--- src/main/resources/createTables.sql
+++ src/main/resources/createTables.sql
@@ -12,6 +12,51 @@ WITH (
 ALTER TABLE execution
   OWNER TO postgres;
 
+CREATE TABLE parameters
+(
+  id serial NOT NULL,
+  parameter text,
+  description text,
+  CONSTRAINT parameters_pk PRIMARY KEY (id),
+  CONSTRAINT parameter_u UNIQUE (parameter)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE parameters
+  OWNER TO postgres;
+
+CREATE TABLE testconfiguration
+(
+id serial NOT NULL,
+tk text,
+description text,
+CONSTRAINT testconf_pk PRIMARY KEY (id)
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE testconfiguration
+OWNER TO postgres;
+
+CREATE TABLE testvalues
+(
+  tk_id integer NOT NULL,
+  parameter_id integer NOT NULL,
+  value text,
+  CONSTRAINT testvalues_pk PRIMARY KEY (tk_id, parameter_id),
+  CONSTRAINT parameter_id_fk FOREIGN KEY (parameter_id)
+      REFERENCES parameters (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT tk_id_fk FOREIGN KEY (tk_id)
+      REFERENCES testconfiguration (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE testvalues
+  OWNER TO postgres;
 
 
 CREATE INDEX execution_project_index ON
